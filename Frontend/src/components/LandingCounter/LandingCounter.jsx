e96154c8837752bf527a100084c5d8419a1d3c10
Ebrahimi.Erfan <ebrahimi.erfan89@gmail.com>
2023-05-19 03:37:57 +0430

    LandingCounter.jsx developed & added to Landing.jsx

---
+++ Frontend/src/components/LandingCounter/LandingCounter.jsx
@@ -0,0 +1,28 @@
+import { useState , useEffect } from "react";
+
+const LandingCounter = ({limitCount}) => {
+
+    const [counter , setCounter] = useState(0)
+
+    useEffect(() => {
+        let interval = setInterval(() => {
+            setCounter(prevCount => prevCount + 1)
+        } , 0.001)
+
+        if(counter === limitCount){
+            clearInterval(interval)
+        }
+
+        return () => clearInterval(interval)
+    } , [counter])
+
+
+
+  return (
+    <span className="landing-status__count">
+        {counter}
+    </span>
+  )
+}
+
+export default LandingCounter;
