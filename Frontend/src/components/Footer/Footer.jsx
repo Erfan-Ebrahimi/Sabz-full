75b0686f72a06db90c10db42d17b3c90118bb7d4
Ebrahimi.Erfan <ebrahimi.erfan89@gmail.com>
2023-06-06 03:41:12 +0430

    Footer.jsx & .scss added

---
+++ Frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,17 @@
+import './Footer.scss'
+
+const Footer = () => {
+  return (
+    <footer class="footer">
+        <div class="container">
+            <div class="footer-widgets">
+                <div class="row">
+                    
+                </div> 
+            </div> 
+        </div> 
+    </footer> 
+  )
+}
+
+export default Footer;
