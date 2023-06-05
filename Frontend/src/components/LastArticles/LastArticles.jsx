72e60292433491ede1adbc2cd5fbb9807e6aeb40
Ebrahimi.Erfan <ebrahimi.erfan89@gmail.com>
2023-06-06 03:15:52 +0430

    ArticleBox added to LastArticles

--- Frontend/src/components/LastArticles/LastArticles.jsx
+++ Frontend/src/components/LastArticles/LastArticles.jsx
@@ -1,5 +1,7 @@
 import './LastArticles.scss';
 import SectionHeader from '../SectionHeader/SectionHeader';
+import ArticleBox from '../ArticleBox/ArticleBox';
+import cover from '../../assets/images/courses/python.png'
 
 
 const LastArticles = () => {
@@ -7,6 +9,26 @@ const LastArticles = () => {
     <div className='articles'>
         <div className="container">
             <SectionHeader title='جدیدترین مقاله ها'  desc='پیش به سوی ارتقای دانش' btnTitle='تمامی مقاله ها'/>
+
+            <div className='articles__content'>
+              <div className="row">
+                <ArticleBox 
+                  title='نحوه نصب کتابخانه در پایتون | آموزش نصب کتابخانه پایتون'
+                  desc='زبان پایتون هم مانند دیگر زبان­های برنامه نویسی رایج، دارای کتابخانه های مختلفی برای تسریع...'
+                  img={cover}
+                />
+                <ArticleBox 
+                  title='نحوه نصب کتابخانه در پایتون | آموزش نصب کتابخانه پایتون'
+                  desc='زبان پایتون هم مانند دیگر زبان­های برنامه نویسی رایج، دارای کتابخانه های مختلفی برای تسریع...'
+                  img={cover}
+                />
+                <ArticleBox 
+                  title='نحوه نصب کتابخانه در پایتون | آموزش نصب کتابخانه پایتون'
+                  desc='زبان پایتون هم مانند دیگر زبان­های برنامه نویسی رایج، دارای کتابخانه های مختلفی برای تسریع...'
+                  img={cover}
+                />
+              </div>
+            </div>
         </div>
     </div>
   )
