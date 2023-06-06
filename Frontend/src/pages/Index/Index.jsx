b8d5f2170bd50d706e3211dd3c4646797c846532
Ebrahimi.Erfan <ebrahimi.erfan89@gmail.com>
2023-06-06 17:49:45 +0430

    Footer.jsx added to Index.jsx

--- Frontend/src/pages/Index/Index.jsx
+++ Frontend/src/pages/Index/Index.jsx
@@ -1,5 +1,6 @@
 // ------------COMPONENTS-------------------//
 import AboutUs from '../../components/AboutUs/AboutUs';
+import Footer from '../../components/Footer/Footer';
 import Header from '../../components/Header/Header';
 import LastArticles from '../../components/LastArticles/LastArticles';
 import LastCourses from '../../components/LastCourses/LastCourses';
@@ -18,6 +19,7 @@ const Index = () => {
       <PopularCourses/>
       <PresellCourses/>
       <LastArticles/>
+      <Footer/>
     </>
   )
 }
