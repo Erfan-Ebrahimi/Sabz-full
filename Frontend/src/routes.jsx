d0e7462607053d7f2910f7c885a808925db22127
Ebrahimi.Erfan <ebrahimi.erfan89@gmail.com>
2023-06-09 03:33:58 +0430

    Category page route added to routes.jsx

--- Frontend/src/routes.jsx
+++ Frontend/src/routes.jsx
@@ -1,7 +1,10 @@
+import Category from './pages/Category/Category';
 import Index from './pages/Index/Index';
 
 const routes = [
-    { path: '/' , element: <Index/> }
+    { path: '/' , element: <Index/> },
+    { path: '/category-info/:categoryName' , element: <Category/> },
+    
 ]
 
 export default routes;
