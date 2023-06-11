625b0efd05bb6752758c22bfb5e208539fd8d322
Ebrahimi.Erfan <ebrahimi.erfan89@gmail.com>
2023-06-11 18:47:47 +0430

    Breadcrumb.jsx & scss create & added to CourseInfo.jsx

--- Frontend/src/pages/CourseInfo/CourseInfo.jsx
+++ Frontend/src/pages/CourseInfo/CourseInfo.jsx
@@ -3,13 +3,20 @@ import './CourseInfo.scss';
 import Footer from '../../components/Footer/Footer';
 import Navbar from '../../components/Navbar/Navbar';
 import Topbar from '../../components/Topbar/Topbar';
-// import Breadcrumb from '../../components/Breadcrumb/Breadcrumb';
+import Breadcrumb from '../../components/Breadcrumb/Breadcrumb';
 
 const CourseInfo = () => {
   return (
     <>
         <Topbar/>
         <Navbar/>
+        <Breadcrumb 
+          links={[
+              {id: 1 , title: 'خانه' , to:'' },
+              {id: 2 , title: 'دوره های فرانت' , to:'category-info/fd' },
+              {id: 3 , title: 'دوره سلام' , to:'course-info/fr' }
+          ]}
+        />
         <Footer/>
     </>
   )
