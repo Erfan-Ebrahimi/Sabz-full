52dfd943b1688b9fe4347d95bc16aa9724e8eb9d
Ebrahimi.Erfan <ebrahimi.erfan89@gmail.com>
2023-06-11 18:48:20 +0430

    CourseInfo route added to routes.jsx

--- Frontend/src/routes.jsx
+++ Frontend/src/routes.jsx
@@ -1,9 +1,11 @@
 import Category from './pages/Category/Category';
+import CourseInfo from './pages/CourseInfo/CourseInfo';
 import Index from './pages/Index/Index';
 
 const routes = [
     { path: '/' , element: <Index/> },
     { path: '/category-info/:categoryName' , element: <Category/> },
+    { path: '/course-info/:courseName' , element: <CourseInfo/> },
     
 ]
 
