0dc90ac3a32cb0a4639776f27effb17f63453d55
Ebrahimi.Erfan <ebrahimi.erfan89@gmail.com>
2023-06-14 20:07:51 +0430

    add courses route to routes.jsx

--- Frontend/src/routes.jsx
+++ Frontend/src/routes.jsx
@@ -1,10 +1,12 @@
 import ArticleInfo from './pages/ArticleInfo/ArticleInfo';
 import Category from './pages/Category/Category';
 import CourseInfo from './pages/CourseInfo/CourseInfo';
+import Courses from './pages/Courses/Courses';
 import Index from './pages/Index/Index';
 
 const routes = [
     { path: '/' , element: <Index/> },
+    { path: '/courses' , element: <Courses/> },
     { path: '/category-info/:categoryName' , element: <Category/> },
     { path: '/course-info/:courseName' , element: <CourseInfo/> },
     { path: '/article-info/:articleName' , element: <ArticleInfo/> },
