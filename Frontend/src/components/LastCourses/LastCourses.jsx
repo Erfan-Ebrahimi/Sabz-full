c5129097f9ae649a99cc4f6f82010295b56a54a8
Ebrahimi.Erfan <ebrahimi.erfan89@gmail.com>
2023-06-14 20:07:01 +0430

    add btnHref to LastCourses.jsx

--- Frontend/src/components/LastCourses/LastCourses.jsx
+++ Frontend/src/components/LastCourses/LastCourses.jsx
@@ -3,29 +3,30 @@ import SectionHeader from '../SectionHeader/SectionHeader';
 import './LastCourses.scss';
 
 const LastCourses = () => {
-  return (
-    <div className="courses">
-        <div className="container">
-            <SectionHeader 
-                title='جدیدترین دوره ها'
-                desc='سکوی پرتاب شما به  سمت موفقیت'
-                btnTitle=' تمام دوره ها'
-            />
-            <div class="courses-content">
-                <div class="container">
-                    <div class="row">
-                        <CourseBox/>
-                        <CourseBox/>
-                        <CourseBox/>
-                        <CourseBox/>
-                        <CourseBox/>
-                        <CourseBox/>
+    return (
+        <div className="courses">
+            <div className="container">
+                <SectionHeader
+                    title='جدیدترین دوره ها'
+                    desc='سکوی پرتاب شما به  سمت موفقیت'
+                    btnTitle=' تمام دوره ها'
+                    btnHref='courses'
+                />
+                <div class="courses-content">
+                    <div class="container">
+                        <div class="row">
+                            <CourseBox />
+                            <CourseBox />
+                            <CourseBox />
+                            <CourseBox />
+                            <CourseBox />
+                            <CourseBox />
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>  
-  )
+    )
 }
 
 export default LastCourses;
