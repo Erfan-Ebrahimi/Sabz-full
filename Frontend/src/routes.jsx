64c21f62c566181b21cf1918bae106504ac2bc53
Ebrahimi.Erfan <ebrahimi.erfan89@gmail.com>
2023-06-14 17:49:13 +0430

    add article-info route to routes.jsx

--- Frontend/src/routes.jsx
+++ Frontend/src/routes.jsx
@@ -1,3 +1,4 @@
+import ArticleInfo from './pages/ArticleInfo/ArticleInfo';
 import Category from './pages/Category/Category';
 import CourseInfo from './pages/CourseInfo/CourseInfo';
 import Index from './pages/Index/Index';
@@ -6,6 +7,7 @@ const routes = [
     { path: '/' , element: <Index/> },
     { path: '/category-info/:categoryName' , element: <Category/> },
     { path: '/course-info/:courseName' , element: <CourseInfo/> },
+    { path: '/article-info/:articleName' , element: <ArticleInfo/> },
     
 ]
 
