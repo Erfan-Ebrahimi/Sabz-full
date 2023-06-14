679967130d09876ce16915989192695f4f2d8583
Ebrahimi.Erfan <ebrahimi.erfan89@gmail.com>
2023-06-14 21:23:03 +0430

    add login route & register route to routes.jsx

--- Frontend/src/routes.jsx
+++ Frontend/src/routes.jsx
@@ -3,6 +3,8 @@ import Category from './pages/Category/Category';
 import CourseInfo from './pages/CourseInfo/CourseInfo';
 import Courses from './pages/Courses/Courses';
 import Index from './pages/Index/Index';
+import Login from './pages/Login/Login';
+import Register from './pages/Register/Register';
 
 const routes = [
     { path: '/', element: <Index /> },
@@ -10,6 +12,8 @@ const routes = [
     { path: '/category-info/:categoryName', element: <Category /> },
     { path: '/course-info/:courseName', element: <CourseInfo /> },
     { path: '/article-info/:articleName', element: <ArticleInfo /> },
+    { path: '/login', element: <Login /> },
+    { path: '/register', element: <Register /> },
 
 ]
 
