16ccd0703dfac702746e4d8e6ac811a91fb45e4e
Ebrahimi.Erfan <ebrahimi.erfan89@gmail.com>
2023-06-17 19:48:12 +0430

    add AuthContext to App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,15 +1,47 @@
+import { useContext, useState } from 'react';
 import { useRoutes } from 'react-router-dom';
 import routes from './routes';
 import './App.css'
+import AuthContext from './context/AuthContext';
 
 function App() {
 
+  // ---------------global states send to Context
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+  const [token, setToken] = useState(false)
+  const [userInfos, setUserInfos] = useState({})
+
+  // ---------------global functions send to Context
+  const login = (token) => {
+    console.log('login');
+    setToken(token)
+    localStorage.setItem('user', JSON.stringify({ token })) //token:token =>token
+  }
+
+  const logout = () => {
+    console.log('logout');
+    setToken(null)
+    setUserInfos({})
+    localStorage.removeItem('user')
+
+  }
+
+
+  // ----------------ROUTES
   const router = useRoutes(routes)
 
   return (
-    <>
+    <AuthContext.Provider value={
+      {
+        isLoggedIn,
+        token,
+        userInfos,
+        login,
+        logout
+      }
+    }>
       {router}
-    </>
+    </AuthContext.Provider>
   )
 }
 
