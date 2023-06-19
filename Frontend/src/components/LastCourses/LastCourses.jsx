b0d448b8dabd2ae9e2b0fc0cf0a156967057c078
Ebrahimi.Erfan <ebrahimi.erfan89@gmail.com>
2023-06-19 18:21:03 +0430

    config alert for Ctegory.jsx

--- Frontend/src/components/LastCourses/LastCourses.jsx
+++ Frontend/src/components/LastCourses/LastCourses.jsx
@@ -4,14 +4,14 @@ import SectionHeader from '../SectionHeader/SectionHeader';
 import './LastCourses.scss';
 
 const LastCourses = () => {
-    const [courses , setCourses] = useState([])
+    const [courses, setCourses] = useState([])
 
     // ------get all courses from api
     useEffect(useCallback(() => {
         fetch('http://localhost:4000/v1/courses')
-        .then(res => res.json())
-        .then(allCourses => setCourses(allCourses))
-    }),[])
+            .then(res => res.json())
+            .then(allCourses => setCourses(allCourses))
+    }), [])
 
     return (
         <div className="courses">
@@ -25,7 +25,7 @@ const LastCourses = () => {
                 <div className="courses-content">
                     <div className="container">
                         <div className="row">
-                            {courses.splice(0,6).map(course => (<CourseBox key={course._id} {...course}/>))}
+                            {courses.splice(0, 6).map(course => (<CourseBox key={course._id} {...course} />))}
                         </div>
                     </div>
                 </div>
--- Frontend/src/pages/Category/Category.jsx
+++ Frontend/src/pages/Category/Category.jsx
@@ -22,61 +22,71 @@ const Category = () => {
         setCourses(allCourses)
         console.log(allCourses);
       })
-  } , [categoryName])
+  }, [categoryName])
   return (
     <>
       <Topbar />
       <Navbar />
 
-      <section className="courses">
-        <div className="container">
-          <div className="courses-top-bar">
-
-            <div className="courses-top-bar__right">
-              <div className="courses-top-bar__row-btn courses-top-bar__icon--active">
-                <i className="fas fa-border-all courses-top-bar__icon"></i>
-              </div>
-              <div className="courses-top-bar__column-btn">
-                <i className="fas fa-align-left courses-top-bar__icon"></i>
-              </div>
-
-              <div className="courses-top-bar__selection">
-                <span className="courses-top-bar__selection-title">
-                  مرتب سازی پیش فرض
-                  <i className="fas fa-angle-down courses-top-bar__selection-icon"></i>
-                </span>
-                <ul className="courses-top-bar__selection-list">
-                  <li className="courses-top-bar__selection-item courses-top-bar__selection-item--active">مرتب سازی پیش فرض</li>
-                  <li className="courses-top-bar__selection-item">مربت سازی بر اساس محبوبیت</li>
-                  <li className="courses-top-bar__selection-item">مربت سازی بر اساس امتیاز</li>
-                  <li className="courses-top-bar__selection-item">مربت سازی بر اساس آخرین</li>
-                  <li className="courses-top-bar__selection-item">مربت سازی بر اساس ارزان ترین</li>
-                  <li className="courses-top-bar__selection-item">مربت سازی بر اساس گران ترین</li>
-                </ul>
-              </div>
-            </div>
-
-            <div className="courses-top-bar__left">
-              <form action="#" className="courses-top-bar__form">
-                <input type="text" className="courses-top-bar__input" placeholder="جستجوی دوره ..." />
-                <i className="fas fa-search courses-top-bar__search-icon"></i>
-              </form>
-            </div>
-
-          </div>
-        </div>
-      </section>
 
       <div className="courses-content">
         <div className="container">
           <div className="row">
-            {courses.map(course => (<CourseBox key={course._id} {...course} />))}
+            {
+              courses.length === 0 ?
+                (<div className='alert alert-warning'>هنوز برای این دسته بندی دوره ایی موجود نمی باشد !!</div>)
+                :
+                (
+                  <>
+                    <section className="courses">
+                      <div className="container">
+                        <div className="courses-top-bar">
+
+                          <div className="courses-top-bar__right">
+                            <div className="courses-top-bar__row-btn courses-top-bar__icon--active">
+                              <i className="fas fa-border-all courses-top-bar__icon"></i>
+                            </div>
+                            <div className="courses-top-bar__column-btn">
+                              <i className="fas fa-align-left courses-top-bar__icon"></i>
+                            </div>
+
+                            <div className="courses-top-bar__selection">
+                              <span className="courses-top-bar__selection-title">
+                                مرتب سازی پیش فرض
+                                <i className="fas fa-angle-down courses-top-bar__selection-icon"></i>
+                              </span>
+                              <ul className="courses-top-bar__selection-list">
+                                <li className="courses-top-bar__selection-item courses-top-bar__selection-item--active">مرتب سازی پیش فرض</li>
+                                <li className="courses-top-bar__selection-item">مربت سازی بر اساس محبوبیت</li>
+                                <li className="courses-top-bar__selection-item">مربت سازی بر اساس امتیاز</li>
+                                <li className="courses-top-bar__selection-item">مربت سازی بر اساس آخرین</li>
+                                <li className="courses-top-bar__selection-item">مربت سازی بر اساس ارزان ترین</li>
+                                <li className="courses-top-bar__selection-item">مربت سازی بر اساس گران ترین</li>
+                              </ul>
+                            </div>
+                          </div>
+
+                          <div className="courses-top-bar__left">
+                            <form action="#" className="courses-top-bar__form">
+                              <input type="text" className="courses-top-bar__input" placeholder="جستجوی دوره ..." />
+                              <i className="fas fa-search courses-top-bar__search-icon"></i>
+                            </form>
+                          </div>
+
+                        </div>
+                      </div>
+                    </section>
+                    {courses.map(course => (<CourseBox key={course._id} {...course} />))}
+                    <Pagination />
+
+                  </>
+                )
+            }
 
           </div>
         </div>
       </div>
 
-      <Pagination />
 
       <Footer />
 
