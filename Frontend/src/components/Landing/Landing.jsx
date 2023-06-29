c264d23f3b9b9027e100addc7f51050b9d713957
Ebrahimi.Erfan <ebrahimi.erfan89@gmail.com>
2023-06-29 11:29:54 +0430

    conect Search component to Landing.jsx

--- Frontend/src/components/Landing/Landing.jsx
+++ Frontend/src/components/Landing/Landing.jsx
@@ -2,9 +2,17 @@
 import Typewriter from 'typewriter-effect';
 import LandingCounter from '../LandingCounter/LandingCounter';
 import './Landing.scss';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Landing = () => {
 
+    const [searchValue, setSearchValue] = useState('')
+
+    const navigate = useNavigate()
+    const goToSearchPage = () => {
+        navigate(`/search/${searchValue}`)
+    }
 
     return (
         <section className="landing">
@@ -34,8 +42,14 @@ const Landing = () => {
                 </h2>
 
                 <div className="landing__searchbar">
-                    <input type="text" className="landing__searchbar-input" placeholder="چه چیزی دوست داری یاد بگیری ..." />
-                    <button className="landing__searchbar-btn" type="submit">
+                    <input
+                        type="text"
+                        className="landing__searchbar-input"
+                        placeholder="چه چیزی دوست داری یاد بگیری ..."
+                        value={searchValue}
+                        onChange={(e) => setSearchValue(e.target.value)}
+                    />
+                    <button className="landing__searchbar-btn" type="submit" onClick={goToSearchPage}>
                         <i className="fas fa-search landing__searchbar-icon"></i>
                     </button>
                 </div>
