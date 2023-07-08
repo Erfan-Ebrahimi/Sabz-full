24c697f5b07b3a02eda355cd03374c676739c0f9
Ebrahimi.Erfan <ebrahimi.erfan89@gmail.com>
2023-07-09 00:08:02 +0430

    develope logout user in Sidebar.jsx in UserPanel

--- Frontend/src/components/UserPanel/Sidebar/Sidebar.jsx
+++ Frontend/src/components/UserPanel/Sidebar/Sidebar.jsx
@@ -1,6 +1,38 @@
+import { useContext } from "react";
+import AuthContext from "../../../context/authContext";
+import { Link, useNavigate } from "react-router-dom";
+import swal from "sweetalert";
 import './Sidebar.scss';
 
 const Sidebar = () => {
+
+    const authContext = useContext(AuthContext);
+    const navigate = useNavigate();
+
+    // ---------logout User
+    const logoutUser = (event) => {
+        event.preventDefault();
+
+        swal({
+            title: "آیا از خروج اطمینان داری؟",
+            icon: "warning",
+            buttons: ["نه", "آره"],
+        }).then((result) => {
+            console.log(result);
+            if (result) {
+                swal({
+                    title: "با موفقیت خارج شدید",
+                    icon: "success",
+                    buttons: "اوکی",
+                }).then(() => {
+                    authContext.logout();
+                    navigate("/");
+                });
+            }
+        });
+    };
+
+
     return (
         <div className="col-3">
             <div className="sidebar">
@@ -37,9 +69,9 @@ const Sidebar = () => {
                         </a>
                     </li>
                     <li className="sidebar__item">
-                        <a className="sidebar__link" href="#">
+                        <Link className="sidebar__link"  onClick={logoutUser}>
                             خروج از سیستم
-                        </a>
+                        </Link>
                     </li>
                 </ul>
             </div>
