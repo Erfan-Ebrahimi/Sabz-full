71f7920c85bf5266bec201a8ac9a6987ae410983
Ebrahimi.Erfan <ebrahimi.erfan89@gmail.com>
2023-07-10 01:04:46 +0430

    create TicketBox.jsx & .scss in UserPanel

---
+++ Frontend/src/components/UserPanel/TicketBox/TicketBox.jsx
@@ -0,0 +1,47 @@
+import swal from 'sweetalert';
+import './TicketBox.scss';
+
+const TicketBox = (props) => {
+
+    // --------show ticket body
+    const showTicketBody = (ticketBody) => {
+        swal({
+            title: ticketBody,
+            buttons: 'OK'
+        })
+    };
+
+
+    return (
+        <div className="ticket-content__box">
+            <div className="ticket-content__right">
+                <div className="ticket-content__right-right">
+                    <a className="ticket-content__link" href="#">
+                        {props.title}
+                    </a>
+                    <span className="ticket-content__category">
+                        <i className="fa fa-ellipsis-v ticket-content__icon"></i>
+                        {props.departmentSubID}
+                    </span>
+                </div>
+            </div>
+            <div>
+                <button className='btn btn-primary btn__body' onClick={() => showTicketBody(props.body)}>مشاهده</button>
+            </div>
+            <div className="ticket-content__left">
+                <div className="ticket-content__left-right">
+                    <div className={`ticket-content__condition ${props.answer ? 'ok' : 'no'}`}>
+                        <span className="ticket-content__condition-text">
+                            {props.answer === 0 ? "پاسخ داده نشده" : "پاسخ داده شده"}
+                        </span>
+                    </div>
+                </div>
+                <div className="ticket-content__left-left">
+                    <span className="ticket-content__time">{props.createdAt.slice(0, 10)}</span>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default TicketBox;
